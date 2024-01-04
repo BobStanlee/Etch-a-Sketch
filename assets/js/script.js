b13724327e72ef0b0ec9f267cb05bea676c1beaf
Bob Stanley <stanleyagbontor63@gmail.com>
2024-01-04 16:20:59 +0000

    Added Styling and fixed width and height of squares in the grid container.

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,20 @@ const gridContainer = document.querySelector(".grid-container");
 
 function CreateSquareDiv(grid = 5) {
 
-    for (let i = 0; i <= (grid * grid); i++) {
+    for (let i = 1; i <= (grid * grid); i++) {
         // Create Square Div
         let squareDiv = document.createElement('div');
         
         squareDiv.classList.add("square"); // add a class of square to the square div element
+
+        squareDiv.id = i; //Id to give each square a unique number
         
-        squareDiv.style.width = "calc(100% / 16)";
-        squareDiv.style.height = "calc(100% / 16)";
+        squareDiv.style.width = `${100 / grid}%`;
+        squareDiv.style.height = `${100 / grid}%`;
         squareDiv.style.backgroundColor = "red";
+        squareDiv.style.border = "1px solid black";
         
         gridContainer.appendChild(squareDiv);
-        console.log(squareDiv);
     }
 }
 
